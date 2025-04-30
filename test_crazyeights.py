bcf8cdd233bc65b2fd64c0057d7d629f67209ab3
Glenn-John <john.glenn104@gmail.com>
2025-04-30 16:00:26 -0700

    card tests

--- test_crazyeights.py
+++ test_crazyeights.py
@@ -5,7 +5,19 @@ from player import Player
 from crazyeights import CrazyEights
 
 class Test_Card(unittest.TestCase):
-    pass
+    def test_constructor_number(self):
+        mycard = Card(7, "Spades")
+        self.assertEqual(mycard.value, 7)
+        self.assertEqual(mycard.suit, "Spades")
+
+    def test_constructor_face(self):
+        mycard = Card("King", "Diamonds")
+        self.assertEqual(mycard.value, "King")
+        self.assertEqual(mycard.suit, "Diamonds")
+
+    def test_str(self):
+        mycard = Card(7, "Spades")
+        self.assertEqual(str(mycard), "7 of Spades")
 
 class Test_Deck(unittest.TestCase):
     pass
